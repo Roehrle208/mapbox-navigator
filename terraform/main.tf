8d344abb6ccc7b528ad75f9f626159a0a4f0ea85
Maximilian Röhrle <roehrle208@gmail.com>
2025-07-13 17:48:18 +0200

    Vercel hinzugefügt

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,10 @@ terraform {
       source  = "integrations/github"
       version = "~> 5.0"
     }
+    vercel = {
+      source  = "vercel/vercel"
+      version = "~> 3.8.0"
+    }
   }
 }
 
@@ -20,8 +24,22 @@ provider "github" {
   owner = "Roehrle208"
 }
 
+provider "vercel" {
+  api_token = var.VERCEL_TOKEN
+}
+
 resource "github_repository" "mapbox_repo" {
   name        = "mapbox-navigator"
   description = "Ein Beispiel-Repo über Terraform erstellt"
   visibility  = "public"
+}
+
+resource "vercel_project" "mapbox_ui" {
+  name      = "mapbox-navigator"
+  framework = "vite"
+  
+  git_repository = {
+    type = "github"
+    repo = "Roehrle208/mapbox-navigator"
+  }
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "VERCEL_TOKEN" {
+  description = "API Token für den Zugriff auf Vercel"
+  type        = string
+  sensitive   = true
+}
